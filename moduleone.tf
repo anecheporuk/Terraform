d385a4fe9993b66db97a726e36cb6ad61b725b73
anecheporuk <andrik21@gmail.com>
2018-06-05 01:32:16 +0300

    updated network config

--- moduleone.tf
+++ moduleone.tf
@@ -8,6 +8,16 @@ variable "private_key_path" {}
 variable "key_name" {
   default = "anecheporuk-air"
 }
+variable "availability-zones" {
+  default = [
+    "us-east-2a",
+    "us-east-2b",
+    "us-east-2c",
+    "us-east-2e"
+  ]
+  type = "list"
+}
+
 
 ##################################################################################
 # PROVIDERS
@@ -24,54 +34,44 @@ provider "aws" {
 ##################################################################################
 
 resource "aws_instance" "nat_instance" {
-  ami           = "ami-6a003c0f"
-  instance_type = "t2.micro"
-  key_name      = "${var.key_name}"
-  source_dest_check = "false"
+  ami                     = "ami-021e3167"
+  instance_type           = "t2.micro"
+  key_name                = "${var.key_name}"
+  source_dest_check       = "false"
+  vpc_security_group_ids  = ["${aws_security_group.terraform_SG.id}"]
+  subnet_id               = "${aws_subnet.public-a.id}"
 
   connection {
-    user        = "ubuntu"
+    user        = "ec2-user"
     private_key = "${file(var.private_key_path)}"
   }
   
   tags {
     Name = "nat_instance"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get install nginx -y",
-      "sudo service nginx start"
-    ]
-  }
 }
 
 resource "aws_instance" "simple_instance" {
-  ami           = "ami-6a003c0f"
-  instance_type = "t2.micro"
-  key_name      = "${var.key_name}"
+  ami                     = "ami-922914f7"
+  instance_type           = "t2.micro"
+  key_name                = "${var.key_name}"
+  vpc_security_group_ids  = ["${aws_security_group.terraform_SG.id}"]
+  subnet_id               = "${aws_subnet.private-a.id}"
 
   connection {
-    user        = "ubuntu"
+    user        = "ec2-user"
     private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get install nginx -y",
-      "sudo service nginx start"
-    ]
   }
 }
 
-resource "aws_security_group" "terraform_vpc" {
+resource "aws_security_group" "terraform_SG" {
   name          = "terraform security group"
   vpc_id        = "${aws_vpc.terraform_vpc.id}"
 
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = 0
+    protocol    = "-1"
     cidr_blocks = ["10.0.0.0/26"]
   }
   ingress {
@@ -88,92 +88,100 @@ resource "aws_security_group" "terraform_vpc" {
   }
 }
 
-resource "aws_vpc" "terraform_vpc" {
-  cidr_block = "10.0.0.0/26"
-
-  
-}
-
 resource "aws_internet_gateway" "terraformIGW" {
   
   vpc_id = "${aws_vpc.terraform_vpc.id}"
   
+  tags {
+    Name = "terraformIGW"
+  }
+  
 }
 
-resource "aws_subnet" "Private-a" {
-  vpc_id     = "${aws_vpc.terraform_vpc.id}"
-  cidr_block = "10.0.0.0/28"
-
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = "10.0.0.0/26"
+  
   tags {
-    Name = "Private-a"
+    Name = "terraform_vpc"
   }
 }
 
-resource "aws_subnet" "Private-b" {
-  vpc_id     = "${aws_vpc.terraform_vpc.id}"
-  cidr_block = "10.0.0.16/28"
-
-  tags {
-    Name = "Private-b"
-  }
-}
-
-resource "aws_subnet" "Public-a" {
+resource "aws_subnet" "public-a" {
   vpc_id     = "${aws_vpc.terraform_vpc.id}"
   cidr_block = "10.0.0.32/28"
-
+  map_public_ip_on_launch = "true"
+#  availability_zone = "${var.availability-zones[count.index]}"
+  availability_zone = "us-east-2a"
   tags {
     Name = "Public-a"
   }
 }
 
-resource "aws_subnet" "Public-b" {
+#resource "aws_subnet" "Public-b" {
+#  vpc_id     = "${aws_vpc.terraform_vpc.id}"
+#  cidr_block = "10.0.0.48/28"
+#
+#  tags {
+#    Name = "Public-b"
+#  }
+#}
+
+resource "aws_subnet" "private-a" {
   vpc_id     = "${aws_vpc.terraform_vpc.id}"
-  cidr_block = "10.0.0.48/28"
+  cidr_block = "10.0.0.0/28"
+  map_public_ip_on_launch = "false"
+ # availability_zone = "${var.availability-zones[count.index]}"
+  availability_zone = "us-east-2a"
 
   tags {
-    Name = "Public-b"
+    Name = "terraform_private-a"
   }
 }
 
-resource "aws_route_table" "Private" {
+#resource "aws_subnet" "Private-b" {
+#  vpc_id     = "${aws_vpc.terraform_vpc.id}"
+#  cidr_block = "10.0.0.16/28"
+#
+#  tags {
+#    Name = "terraform_private-b"
+#  }
+#}
+
+resource "aws_route_table" "public-a" {
+  vpc_id     = "${aws_vpc.terraform_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_internet_gateway.terraformIGW.id}"
+  }
+  tags {
+    Name = "terraform_public-a"
+  }
+  
+}
+
+resource "aws_route_table" "private-a" {
   vpc_id     = "${aws_vpc.terraform_vpc.id}"
   route {
     cidr_block = "0.0.0.0/0"
     instance_id = "${aws_instance.nat_instance.id}"
-  }
-
-  tags {
-    Name = "Private-Route-Table"
-  }
-}
-
-resource "aws_route_table" "Public" {
-  vpc_id     = "${aws_vpc.terraform_vpc.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    instance_id = "${aws_instance.nat_instance.id}"
+    #network_interface_id = "${aws_instance.nat_instance.network_interface_id}"
   }
   
   tags {
-    Name = "Public-Route-Table"
+    Name = "terraform_private-a"
   }
+    
 }
 
-
-resource "aws_route_table_association" "Private" {
-  subnet_id = "${aws_subnet.Private-a.id}"
-  route_table_id = "${aws_route_table.Private.id}"
+resource "aws_route_table_association" "public-a" {
+  subnet_id = "${aws_subnet.public-a.id}"
+  route_table_id = "${aws_route_table.public-a.id}"
 }
 
-#resource "aws_route_table_association" "Private" {
-#  subnet_id = "${aws_subnet.Private-b.id}"
-#  route_table_id = "${aws_route_table.Private.id}"
-#}
-
-resource "aws_route_table_association" "Public" {
-  subnet_id = "${aws_subnet.Public-a.id}"
-  route_table_id = "${aws_route_table.Public.id}"
+resource "aws_route_table_association" "private-a" {
+  subnet_id = "${aws_subnet.private-a.id}"
+  route_table_id = "${aws_route_table.private-a.id}"
+  
 }
 
 ##################################################################################
@@ -182,4 +190,5 @@ resource "aws_route_table_association" "Public" {
 
 output "aws_instance_public_dns" {
     value = "${aws_instance.nat_instance.public_dns}"
+    value = "${aws_instance.nat_instance.public_ip}"
 }
